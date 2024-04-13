91b0bb57079e015d7cc2105c55d60e36fbe62405
Shivam Garg <86313699+gargshivam963963@users.noreply.github.com>
2024-04-13 21:11:46 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const dev = {
-    API_URL: "http://localhost:5000"
-    //  API_URL: "https://construction-backend.onrender.com"
+    // API_URL: "http://localhost:5000"
+     API_URL: "https://construction-backend.onrender.com"
 }
 const config = dev
 export default config
